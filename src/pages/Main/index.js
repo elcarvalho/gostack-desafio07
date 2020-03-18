18abe0972a1b56eb0f703119782fd320045e0b5f
elcarvalho <lessa.eric@gmail.com>
2020-03-18 17:02:03 -0300

    chore(pagina inicial): listagem de produtos

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,61 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {RectButton} from 'react-native-gesture-handler';
+import {Text, View} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import {
+  SafeContainer,
+  ProductContainer,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  ButtonContainer,
+  BasketContainer,
+  BasketText,
+  TextButton,
+} from './styles';
+
+const data = {
+  products: [
+    {
+      id: 1,
+      title: 'Tênis de Caminhada Leve e muito Confortável Confortável',
+      price: 'R$ 179,90',
+      image:
+        'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+    },
+    {
+      id: 2,
+      title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+      price: 'R$ 139.90',
+      image:
+        'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+    },
+  ],
+};
 
 export default function Main({navigation}) {
   return (
-    <View>
-      <Text>Página inicial</Text>
-      <RectButton onPress={() => navigation.navigate('Cart')}>
-        <Text>Cart</Text>
-      </RectButton>
-    </View>
+    <SafeContainer>
+      <FlatList
+        data={data.products}
+        keyExtractor={item => String(item.id)}
+        horizontal={true}
+        renderItem={({item}) => (
+          <ProductContainer>
+            <ProductImage source={{uri: item.image}} />
+            <ProductTitle>{item.title}</ProductTitle>
+            <ProductPrice>{item.price}</ProductPrice>
+            <ButtonContainer>
+              <BasketContainer>
+                <Icon name="shopping-basket" size={24} color="#fff" />
+                <BasketText>1</BasketText>
+              </BasketContainer>
+              <TextButton>ADICIONAR</TextButton>
+            </ButtonContainer>
+          </ProductContainer>
+        )}
+      />
+    </SafeContainer>
   );
 }
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components/native';
+import {darken} from 'polished';
+import {RectButton} from 'react-native-gesture-handler';
+
+export const SafeContainer = styled.SafeAreaView`
+  padding: 20px;
+  background: #191929;
+  flex: 1;
+`;
+
+export const ProductContainer = styled.View`
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  align-items: center;
+  width: 220px;
+  height: 358px;
+  margin-right: 15px;
+`;
+
+export const ProductImage = styled.Image`
+  width: 200px;
+  height: 200px;
+`;
+
+export const ProductTitle = styled.Text`
+  font-size: 16px;
+  margin-bottom: 5px;
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: 21px;
+  font-weight: bold;
+  text-align: left;
+  width: 100%;
+  margin-bottom: 5px;
+`;
+
+export const ButtonContainer = styled(RectButton)`
+  background: #7150c1;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  border-radius: 4px;
+`;
+
+export const BasketContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background: ${darken(0.03, '#7159c1')};
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  width: 53px;
+  padding: 8px;
+`;
+
+export const BasketText = styled.Text`
+  color: #fff;
+`;
+
+export const TextButton = styled.Text`
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
+  flex: 1;
+  text-align: center;
+`;
